8d67457264fd954e63ce8f31864ebab1b8644f04
Mateo Leal <mateolegi@gmail.com>
2022-12-10 17:47:22 -0500

    swift-redux

--- Tests/ReduxTests/ReduxTests.swift
+++ Tests/ReduxTests/ReduxTests.swift
@@ -1,11 +1,56 @@
 import XCTest
 @testable import Redux
 
-final class ReduxTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Redux().text, "Hello, World!")
+struct AppState {
+    var count: Int
+}
+extension AppState: State {
+    init() {
+        self.init(count: 0)
     }
+}
+
+enum AppAction {
+    case incrementCount
+}
+
+typealias AppStore = Store<AppState, AppAction>
+
+final class ReduxTests: XCTestCase {
+
+    func testActionDispatch() async {
+        let store = AppStore { state, action in
+            var newState = state
+            switch action {
+            case .incrementCount: newState.count += 1
+            }
+            return newState
+        }
+        let current0 = await store.state.count
+        XCTAssertEqual(current0, 0)
+        await store.dispatch(action: .incrementCount)
+        let current1 = await store.state.count
+        XCTAssertEqual(current1, 1)
+    }
+    
+//    struct ThrowMiddleware<AppAction>: Middleware {
+//        func callAsFunction(action: AppAction) async -> AppAction? {
+//            switch action {
+//            case .incrementCount:
+//            }
+//        }
+//    }
+
+//    func testMiddlewareActionDispatch() async {
+//        var store = AppStore { state, action in
+//            var newState = state
+//            switch action {
+//            case .incrementCount: newState.count += 1
+//            }
+//            return newState
+//        } middleware: {
+//            ThrowMiddleware<AppAction>()
+//        }
+//        XCTAssertThro
+//    }
 }
